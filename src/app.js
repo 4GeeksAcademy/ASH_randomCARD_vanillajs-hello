fafb588f2b7b10f7666c434764d1bfc9bdc0a92e
agusotoholt <168455366+agusotoholt@users.noreply.github.com>
2024-07-03 15:19:07 +0000

    1er commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //defino arrays
+  let palos = ["♦", "♥", "♠", "♣"];
+  let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+
+  //hago funcion que genera random index condicionado a largo de array que entra como parametro del a funcion y devuelve el contenido del array basado en el random index generado
+  function rand(arr) {
+    let i = Math.floor(Math.random() * arr.length);
+    return arr[i];
+  }
+  //genero variables para palos y numeros usando la funcion rand
+  let randomPalos = rand(palos);
+  let randomNumeros = rand(numeros);
+
+  //obtebgo el espacio del html para inyectar las variables de palo y numero
+  let selectTop = document.querySelector("#palo-top");
+  let selectBody = document.querySelector("#number");
+  let selectBottom = document.querySelector("#palo-bottom");
+
+  //aplico colores segun palo seleccionado en la funcion rand
+  if (randomPalos === "♦" || randomPalos === "♥") {
+    selectTop.style.color = "red";
+    selectBottom.style.color = "red";
+  }
+
+  selectTop.innerHTML = randomPalos;
+  selectBody.innerHTML = randomNumeros;
+  selectBottom.innerHTML = randomPalos;
 };
